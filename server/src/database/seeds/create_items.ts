26708361c52237bf6c71b2517e47fde95ad99407
pedrolssilva <pedrolssilva@hotmail.com>
2020-06-04 20:28:35 -0300

    included seeds configuration into knexfile.ts, created seed to provide minimal items to add in create point and created upload folder with images created in create_items seed

---
+++ server/src/database/seeds/create_items.ts
@@ -0,0 +1,12 @@
+import Knew from "knex";
+
+export async function seed(knex: Knew) {
+  await knex("items").insert([
+    { title: "Lampâdas", image: "lampadas.svg" },
+    { title: "Pilhas e Baterias", image: "baterias.svg" },
+    { title: "Papéis e Papelão", image: "papeis-papelao .svg" },
+    { title: "Resíduos Eletrônicos", image: "eletronicos.svg" },
+    { title: "Resíduos Orgânicos", image: "organicos.svg" },
+    { title: "Óleo de Cozinha", image: "oleo.svg" },
+  ]);
+}
