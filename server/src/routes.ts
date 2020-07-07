106c2096ed0c4098008f32f21d69d974c9abe5a1
pedrolssilva <pedrolssilva@hotmail.com>
2020-07-07 20:18:57 -0300

    add multer lib and multerConfig file to manage the upload of images into API at create point, adjusted some endpoints to return the image_url as a link ready to be used.

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,14 @@
 import express from "express";
+import multer from "multer";
+import multerConfig from "./config/multer";
 
 import PointsController from "./controllers/pointsController";
 import ItemsController from "./controllers/itemsController";
 
 const routes = express.Router();
+
+const upload = multer(multerConfig);
+
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
@@ -11,9 +16,9 @@ const itemsController = new ItemsController();
 // update=atualizar registro, delete=deletar registro
 
 routes.get("/items", itemsController.index);
-
-routes.post("/points", pointsController.create);
 routes.get("/points", pointsController.index);
 routes.get("/points/:id", pointsController.show);
+
+routes.post("/points", upload.single("image"), pointsController.create);
 
 export default routes;
